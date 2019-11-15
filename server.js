14fbeea5404b9b8b1a0a8743322817541bd38525
Glitch (fcc-microservice-3-aerlaut) <none>
2019-11-15 00:33:12 +0000

    First commit with Glitch

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var dns = require('dns');
+var BodyParser = require('body-parser');
 
 var cors = require('cors');
 
 var app = express();
-
+app.use(BodyParser.urlencoded({extended: false}))
+        
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
@@ -25,10 +28,53 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
+
+// Create mongoose Schema
+mongoose.connect(process.env.MONGO_URI);
+
+var Schema = mongoose.Schema;
+
+var URLSchema = new Schema({
+  id: {type: Number, required: true},
+  url: {type: String, required: true }
+});
+
+var URL = mongoose.model('URL', URLSchema);
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post('/api/shorturl/new', function (req, res) {
+  
+  let url_check = /^(http(s)?:\/\/)?([a-zA-Z\-]+)?(\.[a-zA-Z\-]+)+(\/([-a-zA-Z0-9@:%_\+.~#?&//=]*))?$/g;
+  
+  if(!url_check.test(req.body.url)) {
+    
+    res.json({"error":"invalid URL"});
+    
+  } else {
+    
+  // check DNS for errors
+  
+    let lookup_str = req.body.url.replace(/^https?:\/\//g, '');
+
+    dns.lookup(lookup_str, function (err, addresses) {
+      if(err) console.error(err.code);
+    });
+
+    let idx; 
+    URL.countDocuments( function (err, count) {
+
+      if (err) console.error("count" + err);
+      idx = count;
+
+      URL.create({ id : idx + 1, url : req.body.url }, function (err, data) {
+        if(err) console.error(err);
+        res.json({"original_url": req.body.url, "short_url": data.id})
+      });
+
+    });
+    
+  }
+
 });
 
 
